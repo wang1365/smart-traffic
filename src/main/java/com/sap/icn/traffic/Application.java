64199bf8b910a86cf9648d10e9a9b3808d522e93
Xiaochuan Wang <xiaochuan.wang02@sap.com>
2017-07-03 13:55:26 +0800

    initiate springboot application

---
+++ src/main/java/com/sap/icn/traffic/Application.java
@@ -0,0 +1,14 @@
+package com.sap.icn.traffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by I321761 on 2017/7/3.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
